98d0d19467a1ac2d398fb410197e943d573ca53b
liihAbreu <49107493+liihAbreu@users.noreply.github.com>
2023-11-16 02:42:36 -0300

    Correção CI

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173/",
     waitForAnimations: false,
     animationDistanceThreshold: 50,
   },
